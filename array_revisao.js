6cf547e94695cd20a18cb1d17227140b35fbd898
Clisman <matheusclisman@live.com>
2021-02-15 17:45:34 -0400

    aula 15/2/2021

---
+++ array_revisao.js
@@ -0,0 +1,33 @@
+// Array
+
+const animals = [
+    'Lion',
+    'Monkey',
+    'Cat'
+]
+
+// acessar valores dentro do Array
+console.log(animals)
+
+// selecionando os elementos. indique sua posição
+console.log(animals[0]) // acessando o primeiro elemento
+
+// descobrindo o tamanho da Array
+console.log(animals.length)
+
+/*
+    Casso tenha um objeto dentro da Array
+
+const animals = [
+    'Lion',
+    'Monkey',
+    {
+        name: 'Cat',
+        age: 3
+    }
+]
+
+// para acessar o objeto dentro da array, indico a posição que se encontra o objeto dentro da array, e então acesso ela adicionando um ponto.
+
+console.log(animals[2].name)
+*/
---
+++ object_revisao.js
@@ -0,0 +1,17 @@
+// Object
+
+const person = {
+    name: 'John',
+    age: 30,
+    weight: 88.6,
+    isAdmin: true
+}
+
+// impressão total
+console.log(person)
+
+// impressão de um item, basta adicionar um ponto, após o objeto
+console.log(person.name)
+
+// usando irterpolação
+console.log(`${person.name} tem ${person.age} anos`)
