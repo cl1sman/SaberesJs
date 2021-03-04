4ad03305907414179410f35b4288bcc09afb997f
Clisman <matheusclisman@live.com>
2021-03-03 23:32:56 -0400

    organizacao

---
+++ learn-js-web-moderno/exercicios-js/fundamentos/organizacao.js
@@ -0,0 +1,9 @@
+// JS é organizado em sentenças de codigos, que podem ou não terminar com ponto e virgula.
+console.log('Sentença de código')
+
+{
+    {
+        console.log('Olá')
+        console.log('Mundo')
+    }
+}
