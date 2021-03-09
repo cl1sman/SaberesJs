b52cfea30fe02a214e68a222498997367e2c78e5
Clisman <matheusclisman@live.com>
2021-03-08 23:07:41 -0400

    8/3/2021

---
+++ learn-js-web-moderno/exercicios-js/fundamentos/numeros_alguns_cuidados.js
@@ -0,0 +1,6 @@
+console.log(7/0) // vai dar infinito.
+console.log('10' / 2) //vai converter, e dá cinco. consigo fazer calculos com strings
+console.log('Show!' * 2) // NaN
+console.log(0.1 + 0.7) // tipos de impressisão
+// console.log(10.toString()) // não dá
+console.log((10.345).toFixed(2))
---
+++ learn-js-web-moderno/exercicios-js/fundamentos/tipagemFraca.js
@@ -0,0 +1,13 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// evite nome genéricos e siglas (escolhas de bons nomes)
+// exemplo
+let valor = ''
+let numero = 1
+let pqp = false // não usar siglas
