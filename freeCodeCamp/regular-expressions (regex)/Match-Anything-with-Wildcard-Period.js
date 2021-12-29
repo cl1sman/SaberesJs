9d0f05c8fb72a4b7abedcde29372c69bf04c1add
Matheus Clisman <matheusclisman@live.com>
2021-12-29 17:56:07 -0400

    period-method

---
+++ freeCodeCamp/regular-expressions (regex)/Match-Anything-with-Wildcard-Period.js
@@ -0,0 +1,5 @@
+let humStr = "I'll hum a song";
+let hugStr = "Bear hug";
+let huRegex = /hu./;
+huRegex.test(hugStr);
+huRegex.test(hugStr);
--- freeCodeCamp/test.js
+++ freeCodeCamp/test.js
@@ -1,18 +1,3 @@
-class Book{
-    constructor(author){
-        this._author = author;
-    }
-
-    get writer(){
-        return this._author;
-    }
-
-    set writer(updatedAuthor){
-        this._author = updatedAuthor;
-    }
-}
-
-const novel = new Book('anonymous');
-console.log(novel.writer);
-novel.writer = 'newAuthor';
-console.log(novel.writer);
+let testStr = "Repeat, Repeat, Repeat";
+let repeatRegex = /Repeat/g;
+console.log(testStr.match(repeatRegex));
